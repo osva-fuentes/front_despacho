b3cf6b7ac8813e6546660c3edc4a0b5e2f38ecdc
SB- Alumno <SB-Alumno@duoc.acad>
2026-06-24 15:39:00 -0400

    Demostracion github Actions

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_VENTAS = "http://localhost:30081/api/v1/ventas";
-export const API_DESPACHOS = "http://localhost:30082/api/v1/despachos";
+//export const API_VENTAS = "http://localhost:30081/api/v1/ventas";
+//export const API_DESPACHOS = "http://localhost:30082/api/v1/despachos";
